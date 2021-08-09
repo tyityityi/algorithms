3013771cd4338aa2f234da8e7d6c37b2e54e1ab3
Tianyi Zheng <zhetz@student.unimelb.edu.au>
2021-08-09 20:54:25 +0800

    0809

--- src/domain/ListNode.java
+++ src/domain/ListNode.java
@@ -7,4 +7,15 @@ public class ListNode {
     public ListNode(int x) {
         val = x;
     }
+
+    private static void printList(ListNode head){
+        StringBuilder sb = new StringBuilder();
+        while(head!=null){
+            sb.append(head.val+"");
+            sb.append(",");
+            head = head.next;
+        }
+        sb.deleteCharAt(sb.length()-1);
+        System.out.println(sb.toString());
+    }
 }
